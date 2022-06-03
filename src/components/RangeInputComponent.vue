868816f6be1ff1040c40a4a5aff5319933091d29
maty <matikster1@wp.pl>
2022-06-03 12:55:56 +0200

    The RangeInputComponent is finished

--- src/components/RangeInputComponent.vue
+++ src/components/RangeInputComponent.vue
@@ -3,21 +3,27 @@
     <label :for="id">{{ labelText }}</label>
     <div class="container">
       <input
-        type="range"
         :id="id"
-        class="inputRange"
         :value="value"
+        type="range"
         :min="min"
         :max="max"
+        class="inputRange"
         @input="handleInput($event.target.value)"
       />
-      <div class="selector" ref="selector">
-        <div class="selectValue"></div>
-        <div class="selectBtn">
-          <span>{{ value }}</span>
+      <div
+        class="selector"
+        :style="{ left: Math.round((this.value / this.max) * 100) + '%' }"
+      >
+        <div class="selectorBtn">
+          <span class="selectorBtnValue">{{ value }}</span>
         </div>
       </div>
-      <div class="progressBar" ref="progressBar"></div>
+      <div
+        class="progressBar"
+        :style="{ width: Math.round((this.value / this.max) * 100) + '%' }"
+      ></div>
+      <span style="display: block; height: 1px"></span>
     </div>
   </div>
 </template>
@@ -37,10 +43,6 @@ export default {
       default: 120,
     },
   },
-  model: {
-    prop: "value",
-    event: "change",
-  },
   data() {
     return {
       id: null,
@@ -48,27 +50,10 @@ export default {
   },
   mounted() {
     this.id = this._uid;
-
-    this.$refs.selector.style.left =
-      Math.round((this.value / this.max) * 100) + "%";
-    this.$refs.progressBar.style.width =
-      Math.round((this.value / this.max) * 100) + "%";
   },
   methods: {
     handleInput(e) {
-      this.$refs.selector.style.left = Math.round((e / this.max) * 100) + "%";
-      this.$refs.progressBar.style.width =
-        Math.round((e / this.max) * 100) + "%";
-
-      this.$emit("change", e);
-    },
-  },
-  watch: {
-    value() {
-      this.$refs.selector.style.left =
-        Math.round((this.value / this.max) * 100) + "%";
-      this.$refs.progressBar.style.width =
-        Math.round((this.value / this.max) * 100) + "%";
+      this.$emit("input", e);
     },
   },
 };
@@ -82,101 +67,93 @@ export default {
 }
 
 .inputRange {
-  -webkit-appearance: none;
-  width: 100%;
-  height: 7px;
-  outline: none;
-  border-radius: 3px;
   position: absolute;
   top: 0;
   left: 0;
+
+  width: 100%;
+  height: 7px;
+  border-radius: 3px;
   margin: 0;
+
+  -webkit-appearance: none;
+  outline: none;
 }
 
 .inputRange::-webkit-slider-thumb {
-  -webkit-appearance: none;
+  position: relative;
   width: 48px;
   height: 48px;
+  -webkit-appearance: none;
   cursor: pointer;
   z-index: 30;
-  position: relative;
 }
 
 .selector {
-  height: 25px;
-  width: 48px;
   position: absolute;
   bottom: 0px;
   left: 50%;
-  /* transform: translateX(-50%); */
+  height: 25px;
+  width: 48px;
   z-index: 2;
 }
 
-.selectBtn {
+.selectorBtn {
+  position: absolute;
+  bottom: -8px;
+
   height: 10px;
   width: 10px;
-  background: #57649c;
-  color: #57649c;
-  position: absolute;
   border-radius: 50%;
-  transform: translateX(-50%);
-  bottom: -9px;
-  transition: 0.15s all ease-in;
   box-sizing: border-box;
+
+  background: #57649c;
+
+  color: #57649c;
+
+  transform: translateX(-50%);
+  transition: 0.15s all ease-in;
 }
 
-.inputRange:hover ~ .selector .selectBtn {
+.inputRange:hover ~ .selector .selectorBtn {
   border-bottom-right-radius: 0;
-  transform: rotate(45deg) scale(3.2);
   color: #ffffff;
+  transform: rotate(45deg) scale(3.2);
 }
 
-.inputRange:active ~ .selector .selectBtn {
+.inputRange:active ~ .selector .selectorBtn,
+.inputRange:active ~ .progressBar {
   border: 1px solid white;
 }
 
-span {
-  display: inline-block;
-  font-size: 5px;
-  width: 100%;
-  height: 100%;
+.selectorBtnValue {
   position: relative;
   top: -9px;
   left: -4px;
+
+  display: inline-block;
+  width: 100%;
+  height: 100%;
+
+  font-size: 5px;
+
   transform: rotate(315deg);
 }
 
-/* .selectValue {
-  width: 20px;
-  height: 12px;
-  position: absolute;
-  top: 0;
-  background: #ffd200;
-  border-radius: 4px;
-  text-align: center;
-  line-height: 45px;
-  font-size: 20px;
-  font-weight: bold;
-  transform: translateX(-50%);
-}
-
-.selectValue::after {
-  content: "";
-  border-top: 8px solid #ffd200;
-  border-left: 10px solid #000;
-  border-right: 10px solid #000;
-  position: absolute;
-  bottom: -14px;
-  left: 0;
-} */
-
 .progressBar {
-  height: 7px;
-  background: #9aa5d4;
-  border-radius: 3px;
   position: absolute;
-  /* transform: translateY(-50%); */
   top: 0;
   left: 0;
+
+  height: 7px;
+  border-radius: 3px;
+  box-sizing: border-box;
+
+  background: rgb(161, 168, 201);
+  background: linear-gradient(
+    90deg,
+    rgba(161, 168, 201, 1) 0%,
+    rgba(87, 100, 156, 1) 100%
+  );
 }
 </style>
